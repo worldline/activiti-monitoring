cf408b6de7dd47a8b3b811cb0ba826ca52e4056d
Saeid Mirzaei <smirzai@gmail.com>
2013-02-07 15:03:49 +0000

    change imageServlet to imageFilter

---
+++ activiti-webapp-monitor/addKermitUser.sql
@@ -0,0 +1,3 @@
+insert into act_id_user (id_, pwd_) values ('kermit', 'kermit');
+insert act_id_group (id_, name_) values ('admin', 'admin');
+insert into act_id_membership(user_id_, group_id_) values ('kermit', 'admin');
